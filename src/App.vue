636a962125824e1e24f3fffac5b98e79d14f8728
Sophie <72505815+SophieHogg@users.noreply.github.com>
2026-03-02 12:34:39 +1100

    create rudimentary form that outputs as expected

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <script setup>
 import { ref, onMounted } from 'vue'
+import CurrencyInput from './components/CurrencyInput.vue'
+import SelectDropdown from './components/SelectDropdown.vue'
 import { getLoanPurposesAsync, getRequestedRepaymentPeriodsAsync, getRequestedTermMonthsAsync } from './services/service'
+import PMT from './utils/PMT'
+
 defineOptions({
   name: 'App',
 })
@@ -8,32 +12,67 @@ defineOptions({
 const loanPurposes = ref([])
 const requestedRepaymentPeriods = ref([])
 const requestedTermMonths = ref([])
+
+const modelValue = ref('')
+const selectedLoanPurpose = ref(null)
+const selectedRepaymentPeriod = ref(null)
+const selectedTermMonth = ref(null)
+const loading = ref(true)
 onMounted(async () => {
-  loanPurposes.value = await getLoanPurposesAsync()
-  requestedRepaymentPeriods.value = await getRequestedRepaymentPeriodsAsync()
-  requestedTermMonths.value = await getRequestedTermMonthsAsync()
+  try {
+    loading.value = true
+    loanPurposes.value = await getLoanPurposesAsync()
+    requestedRepaymentPeriods.value = await getRequestedRepaymentPeriodsAsync()
+    requestedTermMonths.value = await getRequestedTermMonthsAsync()
+  } finally {
+    loading.value = false
+  }
 })
+
+const outputPerPeriod = ref()
+const repaymentPerPeriod = ref()
+
+const handleSubmit = () => {
+  // TODO: Validation and error states
+  repaymentPerPeriod.value = selectedLoanPurpose.value.annualRate / selectedRepaymentPeriod.value.value
+  const unroundedOutput = PMT(repaymentPerPeriod.value, selectedTermMonth.value.value, modelValue.value)
+  outputPerPeriod.value = Math.round(unroundedOutput)
+}
+
 </script>
 
 <template lang="html">
-  <ul>
-    <li
-      v-for="item in loanPurposes"
-      :key="item.value"
+  <div v-if="loading">
+    Loading...
+  </div>
+  <div v-else>
+    <CurrencyInput
+      :model-value="modelValue"
+      @update:model-value="(value) => modelValue = value"
+    />
+    <SelectDropdown
+      v-model="selectedLoanPurpose"
+      :options="loanPurposes"
+      placeholder="Select loan purpose"
+    />
+    <SelectDropdown
+      v-model="selectedRepaymentPeriod"
+      :options="requestedRepaymentPeriods"
+      placeholder="Select repayment period"
+    />
+    <SelectDropdown
+      v-model="selectedTermMonth"
+      :options="requestedTermMonths"
+      placeholder="Select term"
+    />
+    <button
+      @click="handleSubmit"
     >
-      {{ item.label }} - {{ item.annualRate ?? '-' }}
-    </li>
-    <li
-      v-for="item in requestedRepaymentPeriods"
-      :key="item.value"
-    >
-      {{ item.label }} - {{ item.value }}
-    </li>
-    <li
-      v-for="item in requestedTermMonths"
-      :key="item.value"
-    >
-      {{ item.label }} - {{ item.value }}
-    </li>
-  </ul>
+      Submit
+    </button>
+    <p v-if="outputPerPeriod !== undefined">
+      Repayment per period: ${{ -outputPerPeriod }}
+    </p>
+    <p v-if="outputPerPeriod !== undefined && selectedTermMonth.value !== null"> Repayment total: ${{ -outputPerPeriod * selectedTermMonth.value }}</p>
+  </div>
 </template>
--- src/services/service.spec.js
+++ src/services/service.spec.js
@@ -1,4 +1,4 @@
-import { describe } from 'vitest'
+import { describe, it } from 'vitest'
 import {
     getLoanPurposesAsync,
     getRequestedRepaymentPeriodsAsync,
