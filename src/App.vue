553e284dd8d31d0aef7d1f01b8693a2f3acb0c48
Sophie <72505815+SophieHogg@users.noreply.github.com>
2026-03-02 11:02:26 +1100

    adjust app file to call services, print the data to ensure all returns as desired

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,39 @@
 <script setup>
+import { ref, onMounted } from 'vue'
+import { getLoanPurposesAsync, getRequestedRepaymentPeriodsAsync, getRequestedTermMonthsAsync } from './services/service'
 defineOptions({
   name: 'App',
 })
+
+const loanPurposes = ref([])
+const requestedRepaymentPeriods = ref([])
+const requestedTermMonths = ref([])
+onMounted(async () => {
+  loanPurposes.value = await getLoanPurposesAsync()
+  requestedRepaymentPeriods.value = await getRequestedRepaymentPeriodsAsync()
+  requestedTermMonths.value = await getRequestedTermMonthsAsync()
+})
 </script>
 
-<template>
-  <!-- TODO: Complete the coding challenge here! -->
-  <div />
+<template lang="html">
+  <ul>
+    <li
+      v-for="item in loanPurposes"
+      :key="item.value"
+    >
+      {{ item.label }} - {{ item.annualRate ?? '-' }}
+    </li>
+    <li
+      v-for="item in requestedRepaymentPeriods"
+      :key="item.value"
+    >
+      {{ item.label }} - {{ item.value }}
+    </li>
+    <li
+      v-for="item in requestedTermMonths"
+      :key="item.value"
+    >
+      {{ item.label }} - {{ item.value }}
+    </li>
+  </ul>
 </template>
